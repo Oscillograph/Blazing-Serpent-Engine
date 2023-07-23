7b69c614ea8cadb535e61033e1c307b39f98eeb0
Living Oscillograph <totservis@gmail.com>
2023-07-23 14:54:48 +0300

    Preparation to make 2D-renderer

--- Blazing_Serpent_Engine.cpp
+++
@@ -1,9 +0,0 @@
-// BSE
-#include "BLazing_Serpent_Engine.h"
-
-namespace BSE{
-	/*
-	void print(){
-		printf("Welcome to Blazing Serpent Engine world!\n");
-	}*/
-}
--- Blazing_Serpent_Engine.h
+++
@@ -1,7 +0,0 @@
-#ifndef BLAZING_SERPENT_ENGINE_H
-#define BLAZING_SERPENT_ENGINE_H
-
-#include <Core.h>
-#include <Application.h>
-
-#endif
