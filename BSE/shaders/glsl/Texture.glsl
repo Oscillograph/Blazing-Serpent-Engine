741ede54f35f91c14a55e212eac3d137da0337d4
Living Oscillograph <totservis@gmail.com>
2023-09-30 11:58:08 +0300

    buffers in shader

--- BSE/shaders/glsl/Texture.glsl
+++ BSE/shaders/glsl/Texture.glsl
@@ -9,25 +9,32 @@ layout(location = 3) in float a_TextureIndex;
 layout(location = 4) in float a_TilingFactor;
 layout(location = 5) in int a_EntityID;
 
-uniform mat4 u_ViewProjection;
-// uniform mat4 u_Transform;
+layout (std140, binding = 0) uniform Camera
+{
+	mat4 u_ViewProjection;
+} u_Camera;
 
-out vec4 v_Color;
-out vec2 v_TextureCoordinates;
-out float v_TextureIndex;
-out float v_TilingFactor;
-out int v_EntityID;
+struct VertexOutput
+{
+	vec4 Color;
+	vec2 TextureCoordinates;
+	float TextureIndex;
+	float TilingFactor;
+};
+
+layout (location = 0) out VertexOutput Output;
+layout (location = 4) out int v_EntityID;
 
 
 void main(){
-	v_Color = a_Color;
-	v_TextureCoordinates = a_TextureCoordinates;
-	v_TextureIndex = a_TextureIndex;
-	v_TilingFactor = a_TilingFactor;
+	
+	Output.Color = a_Color;
+	Output.TextureCoordinates = a_TextureCoordinates;
+	Output.TextureIndex = a_TextureIndex;
+	Output.TilingFactor = a_TilingFactor;
 	v_EntityID = a_EntityID;
 
-	// gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-	gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+	gl_Position = u_Camera * vec4(a_Position, 1.0);
 }
 
 #type fragment
@@ -35,15 +42,20 @@ void main(){
 layout(location = 0) out vec4 color;
 layout(location = 1) out int entity;
 
-in vec4 v_Color;
-in vec2 v_TextureCoordinates;
-in float v_TextureIndex;
-in float v_TilingFactor;
-in int v_EntityID;
+struct VertexOutput
+{
+	vec4 Color;
+	vec2 TextureCoordinates;
+	float TextureIndex;
+	float TilingFactor;
+};
 
-uniform sampler2D u_Textures[32];
+layout (location = 0) in VertexOutput Input;
+layout (location = 4) in int v_EntityID;
+
+layout (binding = 0) uniform sampler2D u_Textures[32];
 
 void main(){
-	color =  texture(u_Textures[int(v_TextureIndex)], v_TextureCoordinates * v_TilingFactor) * v_Color;
+	color =  texture(u_Textures[int(Input.TextureIndex)], Input.TextureCoordinates * Input.TilingFactor) * Input.Color;
 	entity = v_EntityID;
 }
